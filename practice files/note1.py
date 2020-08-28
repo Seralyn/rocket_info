f07c7801f11688a8824baa3202025721a2f8c6fc
seralyn <seralyncampbell@gmail.com>
2020-08-28 15:27:01 +0900

    syncing files

--- practice files/note1.py
+++ practice files/note1.py
@@ -1,8 +1,37 @@
+import sys
+
+from PyQt5 import QtGui, QtWidgets, QtPrintSupport
 
 
+class App(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # Create some widgets
+        self.setGeometry(500, 500, 300, 300)
+        self.button = QtWidgets.QPushButton(
+            'Print QTextEdit widget (the one below)', self)
+        self.button.setGeometry(20, 20, 260, 30)
+        self.editor = QtWidgets.QTextEdit(
+            'Wow such text why not change me?', self)
+        self.editor.setGeometry(20, 60, 260, 200)
+        self.button.clicked.connect(self.print_widget)
 
-def doIt():
-    for i in range(230):
-        print(f"self.listWidget.item({i}).setHidden(True)")
+    def print_widget(self):
+        # Create printer
+        printer = QtPrintSupport.QPrinter()
+        # Create painter
+        painter = QtGui.QPainter()
+        # Start painter
+        painter.begin(printer)
+        # Grab a widget you want to print
+        screen = self.editor.grab()
+        # Draw grabbed pixmap
+        painter.drawPixmap(10, 10, screen)
+        # End painting
+        painter.end()
 
-doIt()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    gui = App()
+    gui.show()
+    app.exec_()
